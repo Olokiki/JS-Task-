5428be7505f002546eb051327cbf1f67db6a89ef
Olokiki <58627207+Olokiki@users.noreply.github.com>
2020-03-26 23:35:35 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+
+var name = "Ore-Aruwaji Eniola";
+
+var courses = ["HTML", "CSS", "JS", "NodeJS"];
+
+console.log("Name: " + name);
+console.log("Courses: " + courses);
+
+for (var i = 0; i <= 200; i++) {
+	if (i % 2 != 0) {
+		console.log(i);
+	}
+}
+
+
+
